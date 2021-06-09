17cece1656992cb4826eb879d7dba3004f79182b
Oguz Eray Orhan <oguzeray@icloud.com>
2021-06-09 08:15:33 +0300

    Apollo client set up with client side generators

--- src/Contexts/AuthContext.tsx
+++ src/Contexts/AuthContext.tsx
@@ -1,18 +1,28 @@
 import React, { createContext, useContext, useEffect } from 'react';
 
 import { AuthStore } from '../Store/AuthStore';
+import { observer } from 'mobx-react-lite';
+import { useMeQuery } from '../GQL/GraphqlOperations';
 
 const AuthContext = createContext<typeof AuthStore>(AuthStore);
 
 export const useAuthContext = () => useContext(AuthContext);
 
-export const AuthContextProvider: React.FC = ({ children }) => {
+export const AuthContextProvider: React.FC = observer(({ children }) => {
+  const { setUser, token } = useAuthContext();
+  const { data: UserData, refetch } = useMeQuery();
+
+  useEffect(() => {
+    if (UserData?.me || token) {
+      setUser(UserData?.me || null, refetch);
+    }
+  }, [setUser, refetch, UserData, token]);
   useEffect(() => {
     return () => {
       AuthStore.disposeAutorun();
     };
-  }, []);
+  });
   return (
     <AuthContext.Provider value={AuthStore}>{children}</AuthContext.Provider>
   );
-};
+});
--- src/screens/HomeTabs/HomeScreen.tsx
+++ src/screens/HomeTabs/HomeScreen.tsx
@@ -11,23 +11,26 @@ import { useRoute } from '@react-navigation/native';
 interface HomeScreenProps {}
 const HomeScreen: React.FC<HomeScreenProps> = ({}) => {
   const route = useRoute();
-  const { token, setToken } = useAuthContext();
-
+  const { token, logout, Me } = useAuthContext();
   return (
     <>
       <Header title={route.name} />
       <View style={{ flex: 1, backgroundColor: theme.palette.primary[800] }}>
         <ScrollView>
           <Text>HomeScreen</Text>
-          <Text>`token: ${token}`</Text>
+          <Text>`token: {token}`</Text>
           <View style={{ justifyContent: 'center', alignItems: 'center' }}>
             <Button
               mode="contained"
+              labelStyle={{ color: theme.palette.primary[100] }}
               color={theme.palette.accent.default}
-              onPress={() => setToken(null)}
+              onPress={async () => {
+                await logout();
+              }}
             >
               Logout
             </Button>
+            <Text>{JSON.stringify(Me, null, 2)}</Text>
           </View>
         </ScrollView>
       </View>
