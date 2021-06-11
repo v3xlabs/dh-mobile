0150354d860b6e82126652b4ad3db7c55c15fd20
Oguz Eray Orhan <oguzeray@icloud.com>
2021-06-11 23:45:42 +0300

    codePush set up for ios is done

--- ios/dogehouseTvMobile/AppDelegate.m
+++ ios/dogehouseTvMobile/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <CodePush/CodePush.h>
 #import <React/RCTLinkingManager.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -55,7 +56,7 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 - (BOOL)application:(UIApplication *)application
